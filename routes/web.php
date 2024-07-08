4b041cebb99c551bffe9d8a12cb7954fc56d7a13
Belén Itzel Román Cervantes <124020263+BelenRomanCervantes@users.noreply.github.com>
2024-07-08 17:36:50 -0600

    First commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'welcome')->name('home');
+Route::view('/contact','contact')->name('contact');
+Route::get('/blog', [PostController::class, 'index'])->name('blog');
+Route::view('/about', 'about')->name('about');
